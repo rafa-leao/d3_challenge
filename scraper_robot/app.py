11cc0eacdb8d876a78b107b355c24833302a8b35
rafa-leao <rleao53@gmail.com>
2019-05-06 17:30:16 -0300

    the robot is a class now

---
+++ scraper_robot/app.py
@@ -0,0 +1,5 @@
+from scraper.robot import Robot
+
+if __name__ == '__main__':
+    
+    robot = Robot().paths_catcher()
---
+++ scraper_robot/scraper/robot.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import requests
+
+from utils.url_checker import url_checker
+
+class Robot:
+	"""docstring for robot"""
+	def __init__(self, 
+				 url_site = 'https://scrapethissite.com/pages',
+				 url_site_disallowed_path = ['/ ', '/lessons', '/faq'] ):
+
+		self.url_site 				  = url_site
+		self.url_site_disallowed_path = url_site_disallowed_path
+		
+	def paths_catcher(self):
+
+		url_validator = url_checker(self.url_site,
+									self.url_site_disallowed_path)
+
+		if url_validator:
+
+			site_response = requests.get(self.url_site, timeout=5).content
+
+			site_content = BeautifulSoup(site_response, "html.parser")
+
+			site_content_urls = site_content.find_all('a')
+
+			# just show link in the console
+			counter = 1
+			for path in site_content_urls:
+
+				path = path.attrs["href"] if "href" in path.attrs else ''
+
+				print("{} ==> {}".format(counter, path))
+
+				counter += 1
+
+		else:
+
+			print("Sorry, the path in the site wich you trying to access is disallowed!")
