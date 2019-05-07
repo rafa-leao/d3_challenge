9e606d0d6317ad8e628b9fba725b20b3ccb55cb4
rafa-leao <rleao53@gmail.com>
2019-05-07 18:33:50 -0300

    start message and call new method

--- scraper_robot/app.py
+++ scraper_robot/app.py
@@ -4,8 +4,11 @@ from utils.all_site_paths import all_site_paths
 
 if __name__ == '__main__':
 
+	print("Scraping...")
 
-    # This url is alredy with a allowed path. So the entry point to crawl on every page of this site is with this path!
-    paths = Robot('https://scrapethissite.com/pages').paths_catcher()
+	# This url is alredy with a allowed path. So the entry point to crawl on every page of this site is with this path!
+	paths = Robot().paths_catcher()
 
-    print(paths)
+	print(paths)
+
+	print(Robot().path_assets_catcher(paths))
