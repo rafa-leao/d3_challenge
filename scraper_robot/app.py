2c0e627d4d1413788d83e656b52e3f144238db1e
rafa-leao <rleao53@gmail.com>
2019-05-08 21:24:35 -0300

    more logs to increase readability and make it cleaner

--- scraper_robot/app.py
+++ scraper_robot/app.py
@@ -7,12 +7,13 @@ if __name__ == '__main__':
 	print("Scraping...")
 
 	paths = Robot().paths_catcher()
-
-	print("ALL PATHS ====> ")
-	print(paths)
+	
+	print("-*-------------*-------------*-------------*-------------*-------------*-")
+	print("ALL ALLOWED PATHS ==> ", paths)
+	print("-*-------------*-------------*-------------*-------------*-------------*-")
 
 	print(Robot().path_assets_catcher(paths))
 
 	print("-*-------------*-------------*-------------*-------------*-------------*-")
-	print("     Copy this huge return and paste on: http://jsonviewer.stack.hu/     ")
+	print("-*   Copy this huge return and paste on: http://jsonviewer.stack.hu/   *-")
 	print("-*-------------*-------------*-------------*-------------*-------------*-")
