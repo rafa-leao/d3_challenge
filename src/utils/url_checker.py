34b5a309bbf78651f7140251c25040e89dbdc7af
rafa-leao <rleao53@gmail.com>
2019-08-02 18:45:36 -0300

    Remove comment from url_checker

--- src/utils/url_checker.py
+++ src/utils/url_checker.py
@@ -1,17 +1,12 @@
 from urllib.parse import urlparse
 
+
 def url_checker(url_site,
                 url_site_disallowed_path):
 
     validator = False
 
     for site_path in url_site_disallowed_path:
-
-        """
-            For now, this verification seems silly. 
-            However, in the future, this software could receive the site url dynamically. 
-            If so, this robot goes to the path "robots.txt" and see which paths are disallowed!
-        """
 
         if site_path not in urlparse(url_site).path:
 
@@ -20,7 +15,7 @@ def url_checker(url_site,
 
         else:
 
-            validator= False
+            validator = False
             break
 
     return validator
