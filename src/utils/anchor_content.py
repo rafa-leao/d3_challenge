b46d7fe68b337efd2b690e97e668a7f2ac61a424
rafa-leao <rleao53@gmail.com>
2019-08-02 18:36:28 -0300

    Anchor content now filter the allowed paths and return it

--- src/utils/anchor_content.py
+++ src/utils/anchor_content.py
@@ -1,11 +1,25 @@
-def anchor_content(site_content):
+from utils.get_url import get_url
 
-	site_anchors_content = []
+
+def anchor_content(url_site, paths_not_allowed):
+
+	site_content = get_url(url_site)
 
 	links = site_content.find_all('a')
 
+	# get all anchors
+	site_anchors = []
+
 	for link in links:
 		link = link.attrs["href"] if "href" in link.attrs else ''
-		site_anchors_content.append(link)
+		site_anchors.append(link)
 
-	return site_anchors_content
+	# Filter anchors to return only allowed paths
+	paths = []
+
+	for path in site_anchors:
+
+		if path not in paths_not_allowed:
+			paths.append(path)
+
+	return paths
