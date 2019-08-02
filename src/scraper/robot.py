f20437132e1638074db870a4e5074084c182bbce
rafa-leao <rleao53@gmail.com>
2019-08-02 18:38:09 -0300

    Change method to map the site

--- src/scraper/robot.py
+++ src/scraper/robot.py
@@ -1,36 +1,41 @@
 from utils.get_url import get_url
 from utils.url_checker import url_checker
-from utils.path_checker import path_checker
 from utils.anchor_content import anchor_content
-from utils.all_site_paths import all_site_paths
 from utils.assets_from_path import assets_from_path
 
 
 class Robot:
 
-	def __init__(self,
-				 url_site = 'https://scrapethissite.com/pages',
-				 url_site_disallowed_path = ['/ ', '/lessons/', '/faq/']):
+	def __init__(self, url_site, paths_not_allowed):
 
-		self.url_site 				  = url_site
-		self.url_site_disallowed_path = url_site_disallowed_path
+		self.url_site 		   = url_site
+		self.paths_not_allowed = paths_not_allowed
 
 
-	def paths_catcher(self):
+	def map_site(self):
 
-		url_validator = url_checker(self.url_site,
-									self.url_site_disallowed_path)
+		# Checks if the path followed by the given site is not forbidden
+		if url_checker(self.url_site, self.paths_not_allowed):
 
-		if url_validator:
+			paths = anchor_content(self.url_site, self.paths_not_allowed)
 
-			site_anchors_content = anchor_content(get_url(self.url_site))
+			print('We got all the anchors from {}. Now we will crawl through them and map the site!'.format(self.url_site))
+			print("-*-------------*-------------*-------------*-------------*-------------*-------------*-------------*-------------*-")
 
-			print('We got all paths in {}. Now we will crawl through them and map the site!'
-				  .format(self.url_site))
+			# Crawl through the site and map it
+			paths_found = []
 
-			return all_site_paths(
-				path_checker(site_anchors_content, self.url_site_disallowed_path)
-			)
+			for path in paths:
+
+				path_result = anchor_content('https://scrapethissite.com{}'.format(path), self.paths_not_allowed)
+
+				for single_path in path_result:
+
+					if single_path.startswith('/'):
+						paths_found.append(path)
+
+			# remove the mapped site and remove duplicates
+			return list(dict.fromkeys(paths_found))
 
 		print("Sorry, the path in the site which you trying to access is not allowed!")
 
